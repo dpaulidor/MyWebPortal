b4c362063a8eaf7120e4d8dcce1f16441f7eb72f
Danika Paulidor <danikapaulidor@gmail.com>
2025-07-08 13:55:11 -0400

    Initial commit: ASP.NET Core MVC project setup

---
+++ MyWebPortal/MyWebPortal/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MyWebPortal.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
